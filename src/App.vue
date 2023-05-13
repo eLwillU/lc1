7cafe99bd0b2a2b743f12073ff87b8e1a6dcf73f
Dominic <dominic.willi@students.bfh.ch>
2023-05-13 09:38:04 +0200

    Push

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,6 @@
 <template>
 
-    <v-main>
-    <v-app-bar
-            color="primary"
-            density="compact"
-    >
-        <v-app-bar-title>Photos</v-app-bar-title>
-
-    </v-app-bar>
-
-    </v-main>
-    <v-app-bar>
-        Gee
-    </v-app-bar>
-
-    <v-btn>Yeet</v-btn>
-    <v-btn>Yeet</v-btn>
-    <v-btn>Yeet</v-btn>
-  <!--    <v-app-bar>-->
-  <!--        <router-link to="/">HOME</router-link>-->
-  <!--        <router-link to="/about">ABOUT</router-link>-->
-  <!--        <router-link to="/blog">BLOG</router-link>-->
-  <!--    </v-app-bar>-->
-
-  <!--    <p>-->
-  <!--        <router-view></router-view>-->
-  <!--    </p>-->
+  <h1>Hello World</h1>
 </template>
 
 <script setup>
